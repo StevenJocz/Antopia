bc4b3014e45c65665381eca2f7b4b0831cce7457
root <steven-jocz@hotmail.com>
2023-09-09 21:32:30 -0500

    Ajustes

--- src/components/Logout/Logout.tsx
+++ src/components/Logout/Logout.tsx
@@ -1,18 +1,16 @@
-import { useDispatch } from 'react-redux';
+
 import { useNavigate } from 'react-router-dom';
-import { PrivateRoutes} from '../../models';
-import { resetUser, UserKey } from '../../redux/states/user';
+import { PublicRoutes } from '../../models';
+import {  UserKey } from '../../redux/states/user';
 import { clearLocalStorage } from '../../utilities';
 import { IonIcon } from '@ionic/react';
 import {exitOutline } from 'ionicons/icons';
 
 function Logout() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const logOut = () => {
     clearLocalStorage(UserKey);
-    dispatch(resetUser());
-    navigate(PrivateRoutes.User, { replace: true });
+    navigate(PublicRoutes.Home, { replace: true });
   };
   return (
     <div className='MiPerfil_Contenido_text' onClick={logOut}>
