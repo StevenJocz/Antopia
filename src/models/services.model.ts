cf45bcc628393f0bb02f7ee322778e94dfaa5499
Betsabe Junior Hoyos Barrios <betsabehoyos@gmail.com>
2023-09-24 12:04:13 -0500

    Update services.model.ts

--- src/models/services.model.ts
+++ src/models/services.model.ts
@@ -1,5 +1,6 @@
 export const services = {
     //local: 'https://antopiaapi.azurewebsites.net/api/',
-    local: 'http://localhost:5239/api/',
+    //local: 'http://localhost:5239/api/',
+    local: 'http://localhost:7030/api/', // Betsabe
     produccion: 'https://antopiaapi.azurewebsites.net/api/',
 };
