4f6d2d74d4c99329b7f484913e9874a45587b8b9
Steven Jocz <56320070+StevenJocz@users.noreply.github.com>
2025-03-21 11:05:14 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : 'https://stevenjocz.github.io/Antopia'
+  base: '/Antopia/' // Asegúrate de que sea el nombre correcto de tu repositorio
 })
